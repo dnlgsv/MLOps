5b2d763798624d9f110420081667676afe417dc6
dnlgsv <56847497+dnlgsv@users.noreply.github.com>
2022-01-30 22:01:31 +0300

    Update test_project.py

--- data_governance_hw/test_project.py
+++ data_governance_hw/test_project.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 def test_dataset_existence():
-  assert Path("/data_governance_hw/wine_quality.csv").is_file(), "There is no dataset 'wine_quality.csv' to apply model on."
+  assert Path("data_governance_hw/wine_quality.csv").is_file(), "There is no dataset 'wine_quality.csv' to apply model on."
