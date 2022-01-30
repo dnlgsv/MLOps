50c0e2c003fd084f5a64a8ab571b41edef255100
dnlgsv <56847497+dnlgsv@users.noreply.github.com>
2022-01-30 22:16:44 +0300

    Update test_project.py

--- data_governance_hw/test_project.py
+++ data_governance_hw/test_project.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 def test_dataset_existence():
   assert Path("data_governance_hw/wine_quality.csv").is_file(), "There is no dataset 'wine_quality.csv' to apply model on."
+
+  def test_a_and_b():
+  assert 5+5==10, "Math is broken!"
